560e081b3121e726857ad6f7f423600e460f43d2
Sanjib Paul <1722036@iub.edu.bd>
2021-06-04 02:41:20 +0600

    register and login validation added

--- routes/auth.js
+++ routes/auth.js
@@ -1,38 +1,57 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../model/UserModel");
-const Joi = require("joi");
+const bcrypt = require("bcryptjs")
+const {
+  registerValidation,
+  loginValidation,
+} = require("../validation/validation");
 
-//Schema
-const schema = Joi.object({
-  name: Joi.string().min(6).required(),
-  email: Joi.string().min(10).required(),
-  password: Joi.string().min(8).required(),
+router.post("/register", async (req, res) => {
+  //Validation
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //Check if user already exists
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exists");
+
+  //Hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+  //Create New User
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashPassword,
+  });
+  //Catch Errors
+  try {
+    const newUser = await user.save();
+    res.send({user: user._id});
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
+//Login
+router.post('/login',async (req,res)=>{
+    //Validation
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    //Check if user exists
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("Email doesn't exist");
+  //Password
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if(!validPassword) return res.status(400).send('Invalid Password');
+
+  res.send('Successfully logged in')
 
 
 
-router.post("/register",  (req, res) => {
-    //Validate data before submission
-   const validation =   schema.validate(req.body);
-   res.send(validation)
-
-//   const user = new User({
-//     name: req.body.name,
-//     email: req.body.email,
-//     password: req.body.password,
-//   });
-//   try {
-//     const newUser = await user.save();
-//     res.send(newUser);
-//   } catch (err) {
-//     res.json({ message: err });
-//   }
-});
-
-// router.post('/login',(req,res)=>{
-
-// })
+})
 
 module.exports = router;
